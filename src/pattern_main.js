a7c2b3db3834ce40ba9f2f9249321da3c3218f2f
Sumit Sharma <ssharma@amcbridge.com>
2026-02-19 15:45:07 +0530

    added shader pattern

---
+++ src/pattern_main.js
@@ -0,0 +1,61 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
+import testVertexShader from './shaders/test/pattern/vertex.glsl'
+import testFragmentShader from './shaders/test/pattern/fragment.glsl'
+import './style.css'
+
+const canvas = document.querySelector('canvas.webgl')
+
+const scene = new THREE.Scene()
+
+const geometry = new THREE.PlaneGeometry(1, 1, 32, 32)
+
+const material = new THREE.ShaderMaterial({
+    vertexShader: testVertexShader,
+    fragmentShader: testFragmentShader,
+    side: THREE.DoubleSide
+})
+
+const mesh = new THREE.Mesh(geometry, material)
+scene.add(mesh)
+
+const sizes = {
+    width: window.innerWidth,
+    height: window.innerHeight
+}
+
+window.addEventListener('resize', () =>
+{
+    sizes.width = window.innerWidth
+    sizes.height = window.innerHeight
+
+    camera.aspect = sizes.width / sizes.height
+    camera.updateProjectionMatrix()
+
+    renderer.setSize(sizes.width, sizes.height)
+    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+})
+
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
+camera.position.set(0.25, - 0.25, 1)
+scene.add(camera)
+
+const controls = new OrbitControls(camera, canvas)
+controls.enableDamping = true
+
+const renderer = new THREE.WebGLRenderer({
+    canvas: canvas
+})
+renderer.setSize(sizes.width, sizes.height)
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+const tick = () =>
+{
+    controls.update()
+
+    renderer.render(scene, camera)
+
+    window.requestAnimationFrame(tick)
+}
+
+tick()
